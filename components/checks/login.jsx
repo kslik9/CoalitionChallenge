e3d98c99bd5410ab9b2e8c9aca740f9827c8141a
Redouan Cherrat <recherra@c2r5p2.1337.ma>
2024-01-19 16:27:44 +0100

    preprod

--- components/checks/login.jsx
+++
@@ -1,7 +0,0 @@
-export default function Login() {
-  return (
-    <Link href="/auth" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 hover:bg-gray-50">
-      Log in
-    </Link>
-  )
-}
